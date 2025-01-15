af0d2effeeed240d81f4539f0fde2e7b9e76a81c
AYssu <2997036064@qq.com>
2025-01-15 13:35:05 +0800

    新一代基址扫描工具，优化了对比

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+default_file="fast_scan"
+# 使用默认值
+file_name=${1:-$default_file}
+
+if [ $# -eq 0 ]; then
+    echo "[-] 传入参数: $0 <文件名>"
+    echo "[-] 使用默认文件: $file_name"
+fi
+
+if [ ! -f "$file_name" ]; then
+    echo "[-] 文件 \"$file_name\" 不存在"
+    exit
+fi
+
+pkill $file_name 
+file_path="/data/local/tmp/$file_name"
+cp -r $file_name ${file_path}
+
+# 检查文件是否有执行权限
+if [ ! -x "${file_path}" ]; then
+    echo "[*] 文件 \"$file_name\" 没有执行权限"
+    chmod +x "${file_path}"
+    if [ ! -x "${file_path}" ]; then
+        echo "[-] 给予权限失败 程序退出"
+        exit
+    else
+        echo "[+] 给予权限成功"
+    fi
+fi
+
+echo "[+]=================================="
+su -c ${file_path}
